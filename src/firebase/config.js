a93a82d466d433579a9575dcb3957d1674955f41
rubalwankhede <rubalwankhede@gmail.com>
2026-06-14 21:05:12 +0530

    debug firebase

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,6 +2,10 @@ import { initializeApp } from 'firebase/app'
 import { getAuth, GoogleAuthProvider } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 
+console.log("API KEY:", import.meta.env.VITE_FIREBASE_API_KEY)
+console.log("AUTH DOMAIN:", import.meta.env.VITE_FIREBASE_AUTH_DOMAIN)
+console.log("PROJECT ID:", import.meta.env.VITE_FIREBASE_PROJECT_ID)
+
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
@@ -10,8 +14,3 @@ const firebaseConfig = {
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
 }
-
-const app = initializeApp(firebaseConfig)
-export const auth = getAuth(app)
-export const provider = new GoogleAuthProvider()
-export const db = getFirestore(app)
